b085e4b18507e24aed392fa0a16cee931bd6651e
TOprogram-git <68937114+TOprogram-git@users.noreply.github.com>
2020-08-01 17:06:13 +0900

    Update SequentialSearch_2.c

--- SequentialSearch_2.c
+++ SequentialSearch_2.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define N 18
 int s[N] = {3,4,6,7,9,11,14,15,17,18,20,23,24,26,27,29,30,32}; //配列初期化：今後探索ではこの配列を用いる
@@ -22,5 +21,5 @@ int main(){
     i = SequentialSearch(x);
     if(i == -1) printf("%2dはありませんでした\n",x);
     else printf("%2d番目に%2dを発見\n",i+1,x);
-	return 0;
+    return 0;
 }
