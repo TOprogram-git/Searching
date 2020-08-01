2d2ab76fadd6faa3fb57b06c69b354973478385e
TOprogram-git <68937114+TOprogram-git@users.noreply.github.com>
2020-08-01 17:02:54 +0900

    Add files via upload

---
+++ SequentialSearch_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 18
+int s[N] = {3,4,6,7,9,11,14,15,17,18,20,23,24,26,27,29,30,32}; //配列初期化：今後探索ではこの配列を用いる
+
+int SequentialSearch(int x) //遂次探索
+{
+    int i = 0;
+    do{
+        if(s[i] >= x) break; //昇順ソートになっているため探索する値より大きい値なら探索を中止することで探査時間の短縮可能
+        else i = i + 1;
+    }while(i < N);
+    if(s[i] == x) return i;
+    return -1;
+}
+
+int main(){
+    int x,i;
+    printf("探索する値：");
+    scanf("%d",&x);
+    i = SequentialSearch(x);
+    if(i == -1) printf("%2dはありませんでした\n",x);
+    else printf("%2d番目に%2dを発見\n",i+1,x);
+	return 0;
+}
